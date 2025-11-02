0d8408e389e85954794454c745c31725c7faf88c
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-11-02 22:00:09 +0300

    adding TechStack

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import FeatureCards from './sections/FeatureCards'
 import Hero from './sections/Hero'
 import LogoShowcase from './sections/LogoShowcase'
 import ShowcaseSection from './sections/ShowcaseSection'
+import TechStack from './sections/TechStack'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <LogoShowcase/>
       <FeatureCards/>
       <Experience/>
+      <TechStack/>
     </>
   )
 }
